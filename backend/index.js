91d65850ca1f7c50059015fe45640c041f4f4b76
harshmohite4 <mohiteharsh639@gmail.com>
2025-03-14 00:35:13 +0530

    mongo added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb+srv://Priya:Priya7@cluster0.aklih.mongodb.net/", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
